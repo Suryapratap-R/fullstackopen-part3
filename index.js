159fb962e0c97bdd75ef9ca26f231724b657b273
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-05 07:55:47 +0530

    added gitignore

--- index.js
+++ index.js
@@ -97,6 +97,6 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.debug(`Server is running on port ${PORT}`);
