6e1d4b4764785054e134a6c4d93322a5b60f3f71
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-07 21:52:35 +0530

    Exercise 3.18 Added

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-// app.use(errorHandler)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
